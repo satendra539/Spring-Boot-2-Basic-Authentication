c53a2420810eedae91e6dc15b1591ca3d15dc148
Satendra Chaudhary <satendra.mytoshika@gmail.com>
2020-05-31 11:35:50 +0530

    first commit

---
+++ src/main/java/com/spring/basicauthentication/SpringBootBasicAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.spring.basicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicAuthenticationApplication.class, args);
+	}
+}
